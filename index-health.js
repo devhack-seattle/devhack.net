7c97825b5dcd0cf0be608d4a0dd1ea5aaf2dacc0
maia <maia@devhack.net>
2025-08-23 15:15:56 -0700

    Add healthcheck warning

---
+++ index-health.js
@@ -0,0 +1,30 @@
+(async () => {
+    const el = document.getElementById('service-warning');
+    if (!el) return;
+
+    // checks healthchecks.io to see if any of our important services are down
+    // in the future maybe we do something self-hosted?
+    async function areWeLosing() {
+        const r = await fetch('https://healthchecks.io/b/2/f564cd28-6a7b-474c-bd33-aea7b641d359.json', { cache: 'no-store' });
+        if (!r.ok) throw new Error(r.status + ' ' + r.statusText);
+        const status = await r.json();
+        if (!status || typeof status.down != 'number') {
+            throw new Error('unexpected healthcheck response: ' + JSON.stringify(status));
+        }
+        return status.down > 0;
+    }
+
+    try {
+        const isDown = await areWeLosing();
+        if (isDown) {
+            el.innerHTML = 'oh naur! some space services are currently unreachable, so not all web interactions might work. to get help or report this incident, <a href="#contact">contact us</a>.';
+            el.hidden = false;
+        } else {
+            el.hidden = true;
+        }
+    } catch (e) {
+        // if something goes wrong, no need to alert the visitor since this is just extra credit.
+        console.error('Error checking service health:', e);
+        el.hidden = true;
+    }
+})();
